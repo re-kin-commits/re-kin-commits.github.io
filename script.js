46318c930ea728e4431f95db7ecbc089fcfda341
re-kin-commits <jomaraleta4@gmail.com>
2025-03-30 17:26:13 +0800

    Update script.js

--- script.js
+++ script.js
@@ -46,7 +46,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const ctx = canvasResult.getContext("2d");
 
     const img = new Image();
-    img.src = img.src = "https://github.com/re-kin-commits/re-kin-commits.github.io/blob/main/img.png"; 
+    img.src = img.src = "https://re-kin-commits.github.io/img.png"; 
     ;
 
     img.onload = function() {

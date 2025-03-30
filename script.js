49c95a99c68ce82eb363fab67d51341b076a2bf2
re-kin-commits <jomaraleta4@gmail.com>
2025-03-30 17:15:49 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,59 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    var captures = 0;
+    var captured = 0
+
+    async function camera() {
+        try {
+            const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+            document.getElementById("video").srcObject = stream;
+        } catch (error) {
+            console.error("Error accessing camera: ", error);
+        }
+    }
+
+    function capture() {
+        const video = document.getElementById("video");
+        const canvas = document.getElementById("canvas");
+        const context = canvas.getContext("2d");
+
+        canvas.width = video.videoWidth;
+        canvas.height = video.videoHeight;
+        context.drawImage(video, 0, 0, canvas.width, canvas.height);
+
+        captures++;
+    }
+
+    function timer(seconds) {
+        if (captures < 3) {
+            setTimeout(() => {
+                capture();
+                timer(seconds);
+            }, seconds * 1000);
+        }
+    }
+
+    function start() {
+        captures = 0;
+        timer(3);
+    }
+
+    camera();
+
+
+
+    const canvasResult = document.getElementById("gallery");
+    const ctx = canvasResult.getContext("2d");
+
+    const img = new Image();
+    img.src = img.src = "./Photo/img.png";  // Relative to the HTML file
+    ;
+
+    img.onload = function() {
+        ctx.drawImage(img, 0, 0, 300, 650);
+    }
+
+
+    camera();
+
+});
